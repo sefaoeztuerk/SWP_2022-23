756ba68e88c09054f0c2dcee5c4a6164e93774fc
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2023-01-13 14:39:21 +0100

    done

--- JavaScript/07/script.js
+++ JavaScript/07/script.js
@@ -2,3 +2,21 @@ let random = Math.random() * 100
 let randomAsInteger = Math.round(random);
 console.log(randomAsInteger)
 
+let random1 = Math.random() * 100;
+let randomAsInteger1 = Math.round(random);
+console.log(randomint);
+
+if (randomAsInteger < randomAsInteger1 && randomAsInteger < 50)
+{
+    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini");
+};
+
+if (randomAsInteger < 30 || randomAsInteger1 < 30)
+{
+    console.log("Eine der beiden ist kleiner als 30");
+};
+
+if (randomAsInteger < 50 && randomAsInteger != 50)
+{
+    console.log("Erste Zahl klein, zweite kein 50iger");
+};
---
+++ JavaScript/08_/script.js
@@ -0,0 +1,15 @@
+let randomNumber = Math.round(Math.random() * 5 + 5);
+
+switch (randomNumber)
+{
+    case 10: console.log("Ten"); break;
+    case 9: console.log("Nine"); break;
+    case 8: console.log("Eight"); break;
+    case 7: console.log("Seven"); break;
+    case 6: console.log("Six"); break;
+    case 5: console.log("Five"); break;
+    case 4: console.log("Four"); break;
+    case 3: console.log("Three"); break;
+    case 2: console.log("Two"); break;
+    case 1: console.log("One"); break;
+}
