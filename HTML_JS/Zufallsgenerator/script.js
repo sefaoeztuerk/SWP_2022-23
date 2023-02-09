ad8885cfa017006dd5433f0d1bf40acfd53893e6
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2023-02-09 10:18:36 +0100

    Zufallsgenerator done

---
+++ HTML_JS/Zufallsgenerator/script.js
@@ -0,0 +1,13 @@
+function generate(){
+    console.log("clicked");
+
+    let number1 = document.getElementById("field1").value;
+    let number2 = document.getElementById("field1").value;
+    
+    let amountOfRandomNumbers = parseInt(number2) - parseInt(number1) +1;
+    console.log(number1 + " : " + number2);
+
+    let randomNumber = Math.floor(Math.random() * amountOfRandomNumbers) + parseInt(number1);
+    document.getElementById("randomNumber").innerHTML = randomNumber;
+
+}
