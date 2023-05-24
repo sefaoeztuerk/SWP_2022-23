0889918db3cb35b3eb97b099202d1b045776f5ce
sefaoeztuerk <113421062+sefaoeztuerk@users.noreply.github.com>
2023-05-24 20:36:25 +0200

    array methods started in school finished home

---
+++ Javascript Extended/10 Array Methods/find.js
@@ -0,0 +1,18 @@
+//find
+
+const items = [
+
+    { name: 'Bike',         price: 100   },
+    { name: 'TV',           price: 200   },
+    { name: 'Album',        price: 10    },
+    { name: 'Book',         price: 5     },
+    { name: 'Phone',        price: 500   },
+    { name: 'Computer',     price: 1000  },
+    { name: 'Keyboard',     price: 25    }
+]
+
+const foundItem = items.find((item) =>{
+    return item.name === 'Album'
+})
+
+console.log(foundItem)
